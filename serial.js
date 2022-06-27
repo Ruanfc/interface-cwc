db2b25ac677f110d2baa816d536131c5debeaa43
Ruanfc <ruan_fc@live.com>
2022-06-27 12:45:14 -0300

    Added setTimeout for updating serialPorts
    Probably we'll need to make it inside a worker, and also we'll need
    to filter every useless ports.

--- serial.js
+++ serial.js
@@ -8,21 +8,27 @@ const serialport = new SerialPort({
 // Inicialize serialport select
 var serialSelect = document.getElementById('serial-selector');
 
-SerialPort.list().then((ports) =>
-{
-    ports.forEach(function (port)
+function checkPorts(){
+    SerialPort.list().then((ports) =>
     {
-        // console.log('Port: ' + port.path);
-        const option = document.createElement('option');
-        option.innerHTML = port.path;
-        // console.log("hasChildNodes: " + serialSelect.hasChildNodes());
-        serialSelect.appendChild(option);
-    });
-},
-    (fail) => {
-        console.log("No ports are available.")
-    });
-
+        // Remove every Children
+        while (ports.firstChild) {
+            ports.removeChild(ports.firstChild);}
+        console.log("All children removed");
+        ports.forEach(function (port)
+        {
+            // console.log('Port: ' + port.path);
+            const option = document.createElement('option');
+            option.innerHTML = port.path;
+            // console.log("hasChildNodes: " + serialSelect.hasChildNodes());
+            serialSelect.appendChild(option);
+        });
+    },
+        (fail) => {
+            console.log("No ports are available.")
+        });
+};
+checkPorts();
 
 // Event for Run/stop button
 var runBtn = document.getElementById('run-stop');
@@ -37,3 +43,24 @@ runBtn.addEventListener("click", function(){
         // Stops data capture
     }
 });
+
+// Worker for serialportfunction startWorker() {
+// var w;
+//   if(typeof(Worker) !== "undefined") {
+//     if(typeof(w) == "undefined") {
+//       w = new Worker("demo_workers.js");
+//     }
+//     w.onmessage = function(event) {
+//       document.getElementById("result").innerHTML = event.data;
+//     };
+//   } else {
+//     document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Workers...";
+//   }
+// }
+
+// function stopWorker() {
+//   w.terminate();
+//   w = undefined;
+// }
+
+var timerSerial = setInterval(checkPorts, 10000);
